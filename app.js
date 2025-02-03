20126866cdfa83d91c584854b39c72fa134e9078
anaelisab <anaelisabellone@gmail.com>
2025-02-03 10:45:40 -0300

    cambios estilo alerta

--- app.js
+++ app.js
@@ -115,7 +115,6 @@ function reiniciarJuego() {
   juegoIniciado = false;
   document.getElementById("agregarBtn").removeAttribute("disabled");
   document.getElementById("amigo").removeAttribute("disabled");
-  document.getElementById("sortearBtn").setAttribute("disabled", true);
   document.getElementById("listaAmigos").style.display = "block";
   document.getElementById("listaAmigos").innerHTML = "";
   document.querySelector("h2").textContent = "Digite el nombre de sus amigos";
@@ -126,11 +125,7 @@ function reiniciarJuego() {
 
 function actualizarEstadoJuego() {
   const sortearBtn = document.getElementById("sortearBtn");
-  if (listaAmigos.length >= 3) {
-    sortearBtn.removeAttribute("disabled");
-  } else {
-    sortearBtn.setAttribute("disabled", true);
-  }
+  sortearBtn.disabled = false; // Siempre habilitamos el botón
 }
 
 // Evento para agregar amigo al presionar Enter
